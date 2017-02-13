fb1e17ad86aa5b1585e181afb00e24eb38d41d66
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-13 20:59:35 -0300

    actualizando db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/x'
   root 'pages#x'
+  post 'pages/save_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
